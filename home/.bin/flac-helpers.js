3856aea39d4aae64d48bcafb85f55ddd739ef1d7
Michael P. Geraci <me@mgeraci.com>
2023-06-17 21:48:57 -0500

    consolidate common flac functions

---
+++ home/.bin/flac-helpers.js
@@ -0,0 +1,38 @@
+const execSync = require('child_process').execSync;
+
+const checkForMetaFlac = () => {
+  try {
+    execSync("which metaflac");
+  }
+  catch(err) {
+    console.log("This script requires the `metaflac` package; install it with `brew install flac`.");
+    process.exit(1);
+  }
+};
+
+const escapeFilename = (filename) => {
+  return filename
+    .replace(/ /g, "\\ ")
+    .replace(/'/g, "\\'")
+    .replace(/&/g, "\\&")
+    .replace(/;/g, "\\;")
+    .replace(/\./g, "\\.")
+    .replace(/\$/g, "\\$")
+    .replace(/\(/g, "\\(")
+    .replace(/\)/g, "\\)")
+    .replace(/\|/g, "\\|");
+};
+
+const getTagFromFile = (tag, file) => {
+  return execSync(`metaflac --show-tag=${tag} ${file}`)
+      .toString()
+      .trim()
+      .replace(/^.+=/, '') // remove the tag, format: ARTIST=Deerhoof
+      .replace('/', '-'); // remove invalid filename characters
+};
+
+module.exports = {
+	checkForMetaFlac,
+	escapeFilename,
+	getTagFromFile
+}
